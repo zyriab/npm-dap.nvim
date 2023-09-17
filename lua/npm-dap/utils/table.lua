35f0b34226f99d47c3ab8e80eb86540b12caa9f7
Arthur Wallendorff <hello@metaoist.io>
2023-09-17 17:52:40 +0200

    add functions + refactor

--- lua/npm-dap/utils/table.lua
+++ lua/npm-dap/utils/table.lua
@@ -1,18 +1,38 @@
 local M = {}
 
 
-M.is_table_empty = function(table)
-    return next(table) == nil
+M.is_table_empty = function(t)
+    return next(t) == nil
 end
 
-M.get_keys = function(table)
+M.get_keys = function(t)
     local keys = {}
 
-    for k in pairs(table) do
+    for k in pairs(t) do
         keys[#keys + 1] = k
     end
 
     return keys
+end
+
+M.table_contains = function(t, value)
+    for i = 1, #t do
+        if (t[i] == value) then
+            return true
+        end
+    end
+
+    return false
+end
+
+M.get_index = function(t, value)
+    local index = {}
+
+    for k, v in pairs(t) do
+        index[v] = k
+    end
+
+    return index[value]
 end
 
 return M
